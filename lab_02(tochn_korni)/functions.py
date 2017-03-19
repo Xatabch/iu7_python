e71aa691d798a5f91b4b044e3289054d8cd79f96
Ivan Morozov <ivan.morozov@list.ru>
2017-03-20 00:12:32 +0300

    add visual of graphic.

--- lab_02(tochn_korni)/functions.py
+++ lab_02(tochn_korni)/functions.py
@@ -1,21 +1,33 @@
 import math as m
+import pylab
+from matplotlib import mlab
+import scipy.optimize as sp
+import scipy.misc as misc
 
+#Функция
 def func(x):
-    return m.sin(x)#(x**2 - 3)#(x**3 + 4*x - 3)
+    return (x**2 - 3)#m.sin(x)#(x**2 - 3)#(x**3 + 4*x - 3)
+
+#Производная от функции
 def proizv_func(x):
-    return m.cos(x)#(2 * x)#(3 * x ** 2 + 4)
+    return (2 * x)#m.cos(x)#(2 * x)#(3 * x ** 2 + 4)
+
+#Определение числа от которого начинать уточнение.
 def second_proizv(x):
-    function = m.sin(x)#(x**2 - 3)#x ** 3 + 4 * x - 3
-    second = -m.sin(x)#6 * x
+    function = (x**2 - 3)#m.sin(x)#(x**2 - 3)#x ** 3 + 4 * x - 3
+    second = 2#-m.sin(x)#6 * x
     result = function * second
     if result > 0:
         return 1
     else:
         return 0
+#Формула метода Ньютона.
 def newton1(x):
     if proizv_func(x) != 0:
         x = x - func(x) / proizv_func(x)
     return x
+
+#Уточнение корня
 def utochnenie(proverca,x,eps1,n,number_of_root,pred_znach,sled_znach,error):
     i = 0
     x2 = newton1(x)
@@ -33,6 +45,8 @@ def utochnenie(proverca,x,eps1,n,number_of_root,pred_znach,sled_znach,error):
         function = func(x)
         viv = vivod(x, number_of_root, pred_znach, sled_znach, function, i, error)
         return viv
+
+#Вывод результатов.
 def vivod(x, number_of_root, pred_znach, sled_znach, function, i, error):
     if (x >= 0) and (function > 0):
         print('{:>10} | {:<5}{:>5} | {:>10.7f} | {:<5.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
@@ -44,4 +58,40 @@ def vivod(x, number_of_root, pred_znach, sled_znach, function, i, error):
         print('{:>10} | {:<5}{:>5} | {:^11.7f}|{:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
     elif (x == 0) and (function == 0):
         print('{:>10} | {:<5}{:>5} | {:^11.7f}| {:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+
+def graph(xmin,xmax):
+    xmin = -10
+    xmax = 10
+    h = abs(xmin - xmax)/100
+    dx = 0.01
+
+    xlist = mlab.frange(xmin, xmax, dx)
+    ylist = [func(x) for x in xlist]
+
+    x2 = []
+    y2 = []
+    x3 = []
+    y3 = []
     
+    for i in range(round(abs(xmin-xmax)/h*10)):
+        if abs(misc.derivative(func,xmin+h*i/10)) <= h/10:
+            x2.append(xmin+h*i/10)
+            y2.append(func(xmin+h*i/10))
+        if abs(misc.derivative(func,xmin+h*i/10,1,2)) <= h/10:
+            x3.append(xmin+h*i/10)
+            y3.append(func(xmin+h*i/10)) 
+
+    rootsx = []
+    rootscount = 0
+    while xmin < xmax:
+        if func(xmin) * func(xmin + h) < 0:
+            rootsx.append(sp.ridder(func, xmin ,xmin+h))
+            rootscount += 1
+        xmin += h
+        
+    pylab.plot(xlist, ylist)
+    pylab.grid(True)
+    pylab.plot(rootsx,[0] * rootscount,'bo')
+    pylab.plot(x2,y2,'ro')
+    pylab.plot(x3,y3,'go')
+    pylab.show()
--- lab_02(tochn_korni)/main.py
+++ lab_02(tochn_korni)/main.py
@@ -1,16 +1,23 @@
 #Морозов Иван, ИУ7-24
+#1-ая ч.Уточнение корней методом Ньютона
+#2-ая ч.Вывод графика функции.
 
+#Импорт файла с функциями
 import functions as fc
-#import scipy.optimize as sp
-#import scipy.misc as misc
-#import matplotlib.pyplot as plt
-#import numpy as np
-import math
+import graphic_f as gr
 
-a,b =float(0) , float(10) #map(float,input('Введите начало и конец отрезка: ').split())
-h = 2#float(input("Введите шаг: "))
-n = 100#int(input("Введите максимальное количество итерраций: "))
-eps1 = 0.01#float(input("Введите точность: "))
+#Библиотеки
+import math as m
+import pylab
+from matplotlib import mlab
+import scipy.optimize as sp
+import scipy.misc as misc
+
+#Ввод данных
+a,b =map(float,input('Введите начало и конец отрезка: ').split())
+h = float(input("Введите шаг: "))
+n = int(input("Введите максимальное количество итерраций: "))
+eps1 = float(input("Введите точность: "))
 error = 0
 number_of_root = 0
 print("""
@@ -22,7 +29,7 @@ print("""
 print('{:>10s} | {:<5s}{:>5s}| {:^10s} | {:^9s} | {:^5s} | {:^5} |'.format('№', 'X(n)', 'X(n+1)', 'X', 'f(X)', 'n', 'err'))
 print('         ---------------------------------------------------------')
 
-#Первая часть задания.
+#Первая часть задания(Уточнение корня).
 while a < b:
     sled_znach = a
     pred_znach = sled_znach
@@ -49,34 +56,4 @@ while a < b:
                 
 
 #Вторая часть задания.
-#print()
-#def f(x):
-#    return math.sin(x)#x**2 - 3
-#a,b = map(float,input('Введите начало и конец отрезка: ').split())
-#h = abs(b-a)/100
-#x1 = np.arange (a,b,h/10)
-#x2 = []
-#y2 = []
-#x3 = []
-#y3 = []
-#for i in range(round(abs(a-b)/h*10)):
-#    if abs(misc.derivative(f,a+h*i/10)) <= h/10:
-#        x2.append(a+h*i/10)
-#        y2.append(f(a+h*i/10))
-#    if abs(misc.derivative(f,a+h*i/10,1,2)) <= h/10:
-#        x3.append(a+h*i/10)
-#        y3.append(f(a+h*i/10))        
-#rootsx = []
-#rootscount = 0
-#while a < b:
-#    if f(a)*f(a+h) < 0:
-#        rootsx.append(sp.ridder(f,a,a+h))
-#        rootscount += 1
-#    a += h
-#plt.title('f(x) = $x^2$ - 3')
-#plt.plot(x1,f(x1))
-#plt.plot(rootsx,[0]*rootscount,'bo',label="Корни")
-#plt.plot(x2,y2,'ro',label="Экстремумы")
-#plt.plot(x3,y3,'go',label="Точки перегиба")
-#plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
-#plt.show()
+fc.graph(a,b)
