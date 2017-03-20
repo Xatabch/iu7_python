8232ed88e133a6933d8e2a9e0b70e30337ae3cb7
Ivan Morozov <ivan.morozov@list.ru>
2017-03-20 20:55:44 +0300

    first part is ready

--- lab_02(tochn_korni)/functions.py
+++ lab_02(tochn_korni)/functions.py
@@ -6,16 +6,16 @@ import scipy.misc as misc
 
 #Функция
 def func(x):
-    return (x**2 - 3)#m.sin(x)#(x**2 - 3)#(x**3 + 4*x - 3)
+    return (x**3 - 5*x**2 + 6)#(50*x**3 - 10*x + 3)#m.sin(x)#(x**2 - 3)#(x**3 + 4*x - 3)
 
 #Производная от функции
 def proizv_func(x):
-    return (2 * x)#m.cos(x)#(2 * x)#(3 * x ** 2 + 4)
+    return (3*x**2 - 10*x + 6)#m.cos(x)#(2 * x)#(3 * x ** 2 + 4)
 
 #Определение числа от которого начинать уточнение.
 def second_proizv(x):
-    function = (x**2 - 3)#m.sin(x)#(x**2 - 3)#x ** 3 + 4 * x - 3
-    second = 2#-m.sin(x)#6 * x
+    function = (x**3 - 5*x**2 + 6)#m.sin(x)#(x**2 - 3)#x ** 3 + 4 * x - 3
+    second = 6*x - 10#-m.sin(x)#6 * x
     result = function * second
     if result > 0:
         return 1
@@ -28,7 +28,7 @@ def newton1(x):
     return x
 
 #Уточнение корня
-def utochnenie(proverca,x,eps1,n,number_of_root,pred_znach,sled_znach,error):
+def utochnenie(proverca,x,eps1,n,number_of_root,pred_znach,sled_znach,error,b,h,a):
     i = 0
     x2 = newton1(x)
     while abs(x2-x) > eps1:
@@ -38,30 +38,40 @@ def utochnenie(proverca,x,eps1,n,number_of_root,pred_znach,sled_znach,error):
         if i >= n:
             error = 1
             break
-    if i != 0:
-        if pred_znach == 0 and func(pred_znach) == 0:
-            x = 0.0
-            i = 1
-        function = func(x)
-        viv = vivod(x, number_of_root, pred_znach, sled_znach, function, i, error)
-        return viv
+    #if i == 0:
+        #print(a+h)
+    #if pred_znach == (a + h):
+        #pred_znach == a - h
+    #if (pred_znach == 0 and func(pred_znach) == 0) or (sled_znach == 0 and func(sled_znach) == 0):
+        #x = 0.0
+        #i = 1
+    d = a
+    c = b 
+    function = func(x)
+    viv = vivod(x, number_of_root, pred_znach, sled_znach, function, i, error, d,c)
+    return viv
 
 #Вывод результатов.
-def vivod(x, number_of_root, pred_znach, sled_znach, function, i, error):
-    if (x >= 0) and (function > 0):
-        print('{:>10} | {:<5}{:>5} | {:>10.7f} | {:<5.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
-    elif (x <= 0) and (function < 0):
-        print('{:>10} | {:<5}{:>5} | {:<5.7f} |{:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
-    elif (x <= 0) and (function > 0):
-        print('{:>10} | {:<5}{:>5} | {:<5.7f} | {:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
-    elif (x >= 0) and (function < 0):
-        print('{:>10} | {:<5}{:>5} | {:^11.7f}|{:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
-    elif (x == 0) and (function == 0):
-        print('{:>10} | {:<5}{:>5} | {:^11.7f}| {:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+def vivod(x, number_of_root, pred_znach, sled_znach, function, i, error, d,c):
+    if sled_znach > c:
+        sled_znach = c
+    if pred_znach != sled_znach:
+        if x >= d and x <= c:
+            if (x >= 0) and (function > 0):
+                print('{:>10} | {:<5}{:>5} | {:>10.7f} | {:<5.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+            elif (x <= 0) and (function < 0):
+                print('{:>10} | {:<5}{:>5} | {:<5.7f} |{:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+            elif (x <= 0) and (function > 0):
+                print('{:>10} | {:<5}{:>5} | {:<5.7f} | {:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+            elif (x >= 0) and (function < 0):
+                print('{:>10} | {:<5}{:>5} | {:^11.7f}|{:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+            elif (x == 0) and (function == 0):
+                print('{:>10} | {:<5}{:>5} | {:^11.7f}| {:<0.3e} | {:^5} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, x, function, i, error))
+        else:
+            error = 2
+            print('{:>10} | {:<5}{:>5} | {:^11.7s}| {:^9s} | {:^5s} | {:^5} |'.format(number_of_root, pred_znach, sled_znach, '-', '-', '-', error))
 
 def graph(xmin,xmax):
-    xmin = -10
-    xmax = 10
     h = abs(xmin - xmax)/100
     dx = 0.01
 
@@ -72,6 +82,8 @@ def graph(xmin,xmax):
     y2 = []
     x3 = []
     y3 = []
+    x4 = [xmin,xmax]
+    y4 = [func(xmin),func(xmax)]
     
     for i in range(round(abs(xmin-xmax)/h*10)):
         if abs(misc.derivative(func,xmin+h*i/10)) <= h/10:
@@ -94,4 +106,5 @@ def graph(xmin,xmax):
     pylab.plot(rootsx,[0] * rootscount,'bo')
     pylab.plot(x2,y2,'ro')
     pylab.plot(x3,y3,'go')
+    pylab.plot(x4,y4,'yo')
     pylab.show()
--- lab_02(tochn_korni)/main.py
+++ lab_02(tochn_korni)/main.py
@@ -4,7 +4,7 @@
 
 #Импорт файла с функциями
 import functions as fc
-import graphic_f as gr
+
 
 #Библиотеки
 import math as m
@@ -15,6 +15,8 @@ import scipy.misc as misc
 
 #Ввод данных
 a,b =map(float,input('Введите начало и конец отрезка: ').split())
+d = a
+c = b
 h = float(input("Введите шаг: "))
 n = int(input("Введите максимальное количество итерраций: "))
 eps1 = float(input("Введите точность: "))
@@ -25,35 +27,40 @@ print("""
          0 - нет ошибок
          1 - превышено максимальное число итерраций
          2 - для данной функции метод работает ошибочно
+         (Касательная вышла за пределы отрезка.)
 """)
-print('{:>10s} | {:<5s}{:>5s}| {:^10s} | {:^9s} | {:^5s} | {:^5} |'.format('№', 'X(n)', 'X(n+1)', 'X', 'f(X)', 'n', 'err'))
+print('{:>10s} | {:<5s}{:>5s}| {:^10s} | {:^9s} | {:^5s} | {:^5} |'.format('№', 'X(n)',
+                                                    'X(n+1)', 'X', 'f(X)', 'n', 'err'))
 print('         ---------------------------------------------------------')
 
 #Первая часть задания(Уточнение корня).
-while a < b:
+while a <= b:
     sled_znach = a
     pred_znach = sled_znach
     sled_znach = pred_znach + h
     a += h
     #Проверка на наличие корней на данном отрезке.
-    if (fc.func(pred_znach) * fc.func(sled_znach) < 0) or (fc.func(pred_znach) == 0) or (fc.func(sled_znach) == 0):
+    if (fc.func(pred_znach) * fc.func(sled_znach) < 0) or (fc.func(pred_znach) == 0) \
+       or (fc.func(sled_znach) == 0):
         x0 = pred_znach
         x1 = sled_znach
         proverca_1 = fc.second_proizv(x0)
         proverca_2 = fc.second_proizv(x1)
         #Уточнение корней.
+        #print(proverca_1,proverca_2)
         if proverca_1:
             number_of_root += 1
-            fc.utochnenie(proverca_1,x0,eps1,n,number_of_root,pred_znach,sled_znach,error)
+            fc.utochnenie(proverca_1,x1,eps1,n,number_of_root,pred_znach,sled_znach,error,c,h,d)
             error = 0
         elif proverca_2:
             number_of_root += 1
-            fc.utochnenie(proverca_1,x1,eps1,n,number_of_root,pred_znach,sled_znach,error)
+            fc.utochnenie(proverca_1,x1,eps1,n,number_of_root,pred_znach,sled_znach,error,c,h,d)
             error = 0
         elif not proverca_1 and not proverca_2:
             number_of_root += 1
-            fc.utochnenie(proverca_1,x1,eps1,n,number_of_root,pred_znach,sled_znach,error)
+            #print(x0,x1)
+            fc.utochnenie(proverca_1,x0,eps1,n,number_of_root,pred_znach,sled_znach,error,c,h,d)
                 
 
 #Вторая часть задания.
-fc.graph(a,b)
+fc.graph(d,c)
